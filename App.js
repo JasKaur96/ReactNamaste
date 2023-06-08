e83a25e8c83017cd7b5d9b7ec10e6050f666fba4
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2023-06-08 14:35:00 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,37 @@
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+    xyz: "abc",
+  },
+  React.createElement(
+    "div",
+    {
+      id: "child",
+    },
+    [
+      React.createElement(
+        "h1",
+        {
+          id: "heading",
+          xyz: "abc",
+        },
+        "Hello World !"
+      ),
+      React.createElement(
+        "h1",
+        {
+          id: "heading",
+          xyz: "abc",
+        },
+        "This is Namaste React."
+      ),
+    ]
+  )
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
