6dea1dbbe5b9be079c2c4e01e9c2e43741cede37
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2023-06-09 14:07:15 +0530

    Component Composition

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
-//JSX (transpiled begore it reaches the JS) - Parcel - Babel
-// JSX => Babel transpiles to React.createElement => ReactElement-JS Object => HTMLElement(render)
-const heading = (
-  <h1 className="head" tabIndex="2">
-    Namaste React
+const Title = () => (
+  <h1 className="heading" tabIndex="2">
+    Namaste React !!
   </h1>
+);
+
+//Component Composition - component within a component
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading" tabIndex="2">
+      Functional Component
+    </h1>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
